0da510de231d97f250001496e8353600b51d9743
soyoung-2020 <68388812+soyoung-2020@users.noreply.github.com>
2023-04-20 20:13:20 +0900

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+# Pulls the image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Create a container
+resource "docker_container" "nginxtest" {
+  image = docker_image.nginx.image_id
+  name  = "nginxtest"
+
+}
