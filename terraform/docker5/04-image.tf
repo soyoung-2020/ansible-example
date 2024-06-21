bdbf3a82ba67baa5eb2c2ed59ebf7cc63243c025
soyoung-2020 <68388812+soyoung-2020@users.noreply.github.com>
2024-06-21 19:57:37 +0900

    Update 04-image.tf

--- terraform/docker5/04-image.tf
+++ terraform/docker5/04-image.tf
@@ -12,6 +12,16 @@ provider "docker" {
 #  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
 }
 
-resource "docker_volume" "bookstack_data" {
-  name = "bookstack_data_${terraform.workspace}"
+resource "docker_network" "bookstack_network" {
+    name = "bookstack_network_${terraform.workspace}"
+}
+
+resource "docker_image" "bookstack" {
+    name = "linuxserver/bookstack:21.11.3"
+    keep_locally = true
+}
+
+resource "docker_image" "mariadb" {
+    name = "linuxserver/mariadb:10.5.13"
+    keep_locally = true
 }
