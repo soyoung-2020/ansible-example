6b2c8796f5881bbf473e91fd34cec5142400968a
soyoung-2020 <68388812+soyoung-2020@users.noreply.github.com>
2024-06-21 19:34:20 +0900

    Create main.tf

---
+++ terraform/docker4/main.tf
@@ -0,0 +1,46 @@
+variable wordpress_port {
+  default = "8080"
+}
+
+resource "docker_volume" "db_data" {
+  name = "db_data"
+}
+
+resource "docker_network" "wordpress_net" {
+  name = "wordpress_net"
+}
+
+resource "docker_container" "db" {
+  name  = "db"
+  image = "mysql:8.3.0"
+  restart = "always"
+  network_mode = "wordpress_net"
+  env = [
+     "MYSQL_ROOT_PASSWORD=wordpress",
+     "MYSQL_PASSWORD=wordpress",
+     "MYSQL_USER=wordpress",
+     "MYSQL_DATABASE=wordpress"
+  ]
+  mounts {
+    type = "volume"
+    target = "/var/lib/mysql"
+    source = "db_data"
+    }
+}
+
+resource "docker_container" "wordpress" {
+  name  = "wordpress"
+  image = "wordpress:latest"
+  restart = "always"
+  network_mode = "wordpress_net"
+  env = [
+    "WORDPRESS_DB_HOST=db:3306",
+    "WORDPRESS_DB_USER=wordpress",
+    "WORDPRESS_DB_NAME=wordpress",
+    "WORDPRESS_DB_PASSWORD=wordpress"
+  ]
+  ports {
+    internal = "80"
+    external = "${var.wordpress_port}"
+  }
+}
