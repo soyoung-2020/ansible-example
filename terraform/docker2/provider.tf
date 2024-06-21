eb3ba6c72a5fcece50fc82861afffcc3d7817831
soyoung-2020 <68388812+soyoung-2020@users.noreply.github.com>
2024-06-21 19:19:44 +0900

    Create provider.tf

---
+++ terraform/docker2/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host     = "ssh://vagrant@ansible-node1:22"
+#  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
